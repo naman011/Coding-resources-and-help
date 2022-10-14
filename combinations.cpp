b13f9d7c625e465b602710a6f6f897663291f74f
adarshmohansharma <115030107+adarshmohansharma@users.noreply.github.com>
2022-10-14 21:56:53 +0530

    Create combinations.cpp

---
+++ combinations.cpp
@@ -0,0 +1,43 @@
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n";
+#define PI 3.1415926535
+#define SORT(v)  sort(v.begin(), v.end())
+#define SORTR(v) sort(v.rbegin(), v.rend())
+#define MAX(v) *max_element(v.begin(), v.end())
+#define MIN(v) *min_element(v.begin(), v.end())
+
+typedef long long int ll;
+
+const ll N = 3e5 + 10;
+const ll mod = 1e9 + 7;
+const ll MAXX = 1e6 + 5;
+const ll nn=1e5 + 1;
+
+
+
+class Solution {
+     void combinationSum(vector<int> &candidates, int target, vector<vector<int> > &res, vector<int> &combination, int begin) {
+        if (!target) {
+            res.push_back(combination);
+            return;
+        }
+        for (int i = begin; i != candidates.size() && target >= candidates[i]; ++i) {
+            combination.push_back(candidates[i]);
+            combinationSum(candidates, target - candidates[i], res, combination, i);
+            combination.pop_back();
+        }
+    }
+    
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+         FAST;
+        // ll n=candidates.size();
+        vector<vector<int>>res;
+        vector<int>combination;
+        SORT(candidates);
+        combinationSum(candidates, target, res, combination, 0);
+        return res;
+    }
+};
